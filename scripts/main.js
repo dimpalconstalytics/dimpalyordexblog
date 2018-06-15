48776d15c38eab26e60e7d321627a7e59085db64
Peiwen Lu <hi@peiwen.lu>
2018-06-15 16:27:48 +0800

    Fix mobile menu toggle function

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
- // toggle responsive menu class
-  $(document.body).on('click', '.responsive-dropdown__trigger' ,function(e) {
-     e.stopPropagation();
-     var $responsiveTriggerBtn = $(this);
-     if ($responsiveTriggerBtn.hasClass('open')) {
-       $('html, body').css('overflow', 'visible');
-       $responsiveTriggerBtn.removeClass('open');
-     } else {
-       $('html, body').css('overflow', 'hidden');
-       $responsiveTriggerBtn.addClass('open');
-       $('.responsive-dropdown__menu').animate({scrollTop: 0}, 0);
-     }
+  // toggle responsive menu class
+  $(document.body).on('click', '.c-header__hamburger', function(e) {
+    e.stopPropagation();
+    if ($('html').hasClass('menu-open')) {
+      $('.header__logo').removeClass('header__logo--dark');
+      $('.header__signup')
+        .removeClass('o-primary-btn')
+        .addClass('o-secondary-outline-btn');
+    } else {
+      $('.header__logo').addClass('header__logo--dark');
+      $('.header__signup')
+        .removeClass('o-secondary-outline-btn')
+        .addClass('o-primary-btn');
+    }
+
+    $('html').toggleClass('menu-open');
   });
 });
